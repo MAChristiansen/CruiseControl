a20ceaca7f09e6a8b35f35cdd7f149ae4741c2ee
Marcus Christiansen <mac080497@gmail.com>
2023-10-24 22:33:09 +0200

    Added message to the alert

--- Sources/CruiseControl/CCNavigationView.swift
+++ Sources/CruiseControl/CCNavigationView.swift
@@ -1,9 +1,6 @@
 import Foundation
 import SwiftUI
 
-
-
-
 public struct CCNavigationView<Destination: CCDestination, RootView: View>: View {
     
     @ObservedObject private var navigationStack: CCNavigationStack<Destination>
@@ -31,20 +28,24 @@ public struct CCNavigationView<Destination: CCDestination, RootView: View>: View
             navigationStack.sheet = nil
         }) {
             navigationStack.sheet!.buildView()
-                .alert(navigationStack.alert?.title ?? "", isPresented: $navigationStack.presentSheetAlert) {
-                    ForEach(navigationStack.alert?.buttons ?? []) { button in
-                        Button(button.title, role: button.role) {
-                            button.action()
-                        }
+                .presentAlert(navigationStack.alert, isPresented: $navigationStack.presentSheetAlert)
+        }
+        .presentAlert(navigationStack.alert, isPresented: $navigationStack.presentAlert)
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func presentAlert(_ model: AlertModel?, isPresented: Binding<Bool>) -> some View {
+        self
+            .alert(model?.title ?? "", isPresented: isPresented) {
+                ForEach(model?.buttons ?? []) { button in
+                    Button(button.title, role: button.role) {
+                        button.action()
                     }
                 }
-        }
-        .alert(navigationStack.alert?.title ?? "", isPresented: $navigationStack.presentAlert) {
-            ForEach(navigationStack.alert?.buttons ?? []) { button in
-                Button(button.title, role: button.role) {
-                    button.action()
-                }
+            } message: {
+                Text(model?.message ?? "")
             }
-        }
     }
 }
